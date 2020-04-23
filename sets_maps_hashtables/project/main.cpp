7b9263c63ffcab7057783984cead89a26faa237a
Paul <paul@z.fios-router.home>
2020-04-23 16:27:25 -0700

    update

---
+++ sets_maps_hashtables/project/main.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<list>
+#include<set>
+#include<string>
+
+struct Word {
+   Word() {};
+   Word(std::string w) : word(w){};
+   std::string word;
+   std::list<int> line_numbers;
+};
+
+bool operator==(const Word &lhs,const Word &rhs) {
+   return lhs.word == rhs.word;
+}
+
+bool operator<(const Word &lhs,const Word &rhs) {
+   return lhs.word < rhs.word;
+}
+
+std::ostream& operator<<(std::ostream& ostr,const Word &w) {
+   ostr << w.word;
+   return ostr;
+}
+
+int main()
+{
+    Word w1,w2;
+    std::set<Word> s;
+    w1.word = "one";
+    w2.word = "two";
+    s.insert(w1);
+    s.insert(w2);
+    s.insert(Word("three"));
+    s.insert(Word("four"));
+
+    if(w1 == w2) {
+       std::cout << "They are equal" << std::endl;
+    }
+    else {
+       std::cout << "They are not equal" << std::endl;
+    }
+    std::cout << w1 << std::endl;
+    std::cout << s.size() << std::endl;
+    for(auto word : s)
+    {
+       std::cout << word << std:: endl;
+    }
+    return 0;
+}
